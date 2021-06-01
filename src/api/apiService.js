c2679a205432e83dbbb78b24eb869e843e48c208
faridhash26 <faridhash26@gmail.com>
2021-06-01 12:32:47 +0430

    the better waay

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -5,6 +5,19 @@ import axios from "axios";
 //   return http.get("https://jsonplaceholder.typicode.com/users");
 // };
 
-export const apicall = ({ url, method, body = null, headers = null }) => {
-  return axios[method](url, JSON.parse(headers), JSON.parse(body));
+export const apicall = ({
+  url,
+  method,
+  body = null,
+  headers = null,
+  params = null,
+}) => {
+  // return axios[method](url, headers, JSON.parse(body));
+  return axios({
+    method: method,
+    url: url,
+    headers: headers,
+    data: body,
+    params: params,
+  });
 };
--- src/redux/simpleAction.js
+++ src/redux/simpleAction.js
@@ -5,9 +5,19 @@ export const getuser = () => async (dispatch) => {
   dispatch({ type: GET_USER_REQ });
   try {
     const { data } = await apicall({
-      method: "get",
-      url: "https://jsonplaceholder.typicode.com/users",
+      method: "post",
+      url: "https://jsonplaceholder.typicode.com/posts",
+      body: {
+        id: 1,
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      },
+      headers: {
+        "Content-type": "application/json",
+      },
     });
+    console.log(data);
     dispatch({
       type: GET_USER_SUCCESS,
       payload: data,
